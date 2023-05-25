f4c02047cee8315c48e368eca57eb7f9af7b3b07
naah16 <naarah.oliveira@gmail.com>
2023-05-25 20:29:41 -0300

    refatoring

--- desafio.kt
+++ desafio.kt
@@ -1,25 +1,27 @@
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-data class Usuario(val nome: String)
+data class Aluno(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-    private val inscritos = mutableListOf<Usuario>()
+class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
+    private val inscritos = mutableListOf<Aluno>()
 
-    fun matricular(usuario: Usuario) {
-        if (!inscritos.contains(usuario)) {
-            inscritos.add(usuario)
-            println("Usuário ${usuario.nome} matriculado em $nome.")
-        } else {
-            println("Usuário ${usuario.nome} já está matriculado em $nome.")
+    fun matricular(vararg alunos: Aluno) {
+        for (aluno in alunos) {
+            if (!inscritos.contains(aluno)) {
+                inscritos.add(aluno)
+                println("Aluno ${aluno.nome} matriculado em $nome.")
+            } else {
+                println("Aluno ${aluno.nome} já está matriculado em $nome.")
+            }
         }
     }
 
     fun listarInscritos() {
-        println("Usuários inscritos em $nome:")
-        for (usuario in inscritos) {
-            println(usuario.nome)
+        println("Alunos inscritos na formação $nome:")
+        for (aluno in inscritos) {
+            println(aluno.nome)
         }
     }
 }
@@ -29,16 +31,16 @@ fun main() {
     val conteudo2 = ConteudoEducacional("Algoritmos Avançados", 180)
     val conteudo3 = ConteudoEducacional("Estruturas de Dados", 150)
 
-    val formacao = Formacao("Formação em Programação", listOf(conteudo1, conteudo2, conteudo3))
+    val formacao = Formacao("Formação em Desenvolvimento Web", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2, conteudo3))
 
-    val usuario1 = Usuario("Thiago")
-    val usuario2 = Usuario("Jaque")
-    val usuario3 = Usuario("Fernando")
+    val aluno1 = Aluno("Thiago")
+    val aluno2 = Aluno("Naarah")
+    val aluno3 = Aluno("Fernando")
 
-    formacao.matricular(usuario1) // Usuário João matriculado na formação Formação em Programação.
-    formacao.matricular(usuario2) // Usuário Maria matriculado na formação Formação em Programação.
-    formacao.matricular(usuario1) // Usuário João já está matriculado na formação Formação em Programação.
-    formacao.matricular(usuario3) // Usuário Pedro matriculado na formação Formação em Programação.
+    formacao.matricular(aluno1) 
+    formacao.matricular(aluno2) 
+    formacao.matricular(aluno1) 
+    formacao.matricular(aluno3) 
 
     formacao.listarInscritos()
 }
